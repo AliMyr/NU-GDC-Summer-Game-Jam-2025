9174a3f042dd650e9e5fa076e2e45a918c23970a
AliMyr <rabo41ali@gmail.com>
2025-08-03 02:56:18 +0500

    (ADD) Player Sprint

--- Assets/Scripts/CharacterData.cs
+++ Assets/Scripts/CharacterData.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Configs/CharacterData")]
 public class CharacterData : ScriptableObject
 {
-    [SerializeField] private float moveSpeed;
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float sprintSpeed;
 
-    public float MoveSpeed => moveSpeed;
+    public float WalkSpeed => walkSpeed;
+    public float SprintSpeed => sprintSpeed;
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,17 +8,44 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private CinemachineCamera cinemachineCamera;
     [SerializeField] private CharacterData characterData;
 
+    private float currentSpeed;
+
     private Vector2 move;
+
+    private void Awake()
+    {
+        Initialize(characterData);
+    }
+
+    private void Initialize(CharacterData characterData)
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        currentSpeed = characterData.WalkSpeed;
+    }
 
     public void OnMove(InputValue inputValue) 
     {
         move = inputValue.Get<Vector2>();
     }
 
+    public void OnSprint(InputValue inputValue)
+    {
+        if(inputValue.Get<float>() > 0.5f)
+        {
+            currentSpeed = characterData.SprintSpeed;
+        }
+        else
+        {
+            currentSpeed = characterData.WalkSpeed;
+        }
+    }
+
     private void Update()
     {
         Vector3 direction = (GetForward() * move.y + GetRight() * move.x).normalized;
-        characterController.Move(direction * characterData.MoveSpeed * Time.deltaTime);
+        characterController.Move(direction * currentSpeed * Time.deltaTime);
 
     }
 
